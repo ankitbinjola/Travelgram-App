36ac0f1f29d8532b2f65649f6bfaaf2e9f9c1a33
ankitbinjola <ankitbinjola@Ankits-MacBook-Air.local>
2021-06-26 14:11:22 +0530

    added prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD-OSG41TZ0-T0KUuQBvjkksBKTRBvQy9A",
+    authDomain: "covid-registration-9cde2.firebaseapp.com",
+    databaseURL: "https://covid-registration-9cde2.firebaseio.com",
+    projectId: "covid-registration-9cde2",
+    storageBucket: "covid-registration-9cde2.appspot.com",
+    messagingSenderId: "158189195878",
+    appId: "1:158189195878:web:c783f652d59ffd563053a7"
+}
 };
